6dae4e25d5423523b53525aa735e5bfb0ff30500
dan <ilhyungkim@hotmail.com>
2019-04-27 01:30:06 -0400

    Created class that lists constants

---
+++ app/src/main/java/com/dan/traderevmobilechallenge/globals/Constants.java
@@ -0,0 +1,10 @@
+package com.dan.traderevmobilechallenge.globals;
+
+/**
+ * Created by Dan Kim on 2019-04-27
+ */
+public class Constants {
+
+    public static final String  KEY_CURRENT_POSITION = "key_currentPosition";
+    public static final String  KEY_PHOTOS = "key_photo_list";
+}

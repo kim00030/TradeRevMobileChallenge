0b745b8c32cc47f2ec4456ba0791ffaf6513f302
dan <ilhyungkim@hotmail.com>
2019-04-28 12:36:44 -0400

    Data binded

--- app/src/main/java/com/dan/traderevmobilechallenge/view/fragments/GridViewFragment.java
+++ app/src/main/java/com/dan/traderevmobilechallenge/view/fragments/GridViewFragment.java
@@ -14,8 +14,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
-import com.dan.traderevmobilechallenge.R;
 import com.dan.traderevmobilechallenge.adapters.StaggeredRecyclerViewAdapter;
+import com.dan.traderevmobilechallenge.databinding.FragmentGridBinding;
 import com.dan.traderevmobilechallenge.view.MainActivity;
 import com.dan.traderevmobilechallenge.viewmodel.MainActivityViewModel;
 
@@ -26,29 +26,30 @@ import java.util.Objects;
  */
 public class GridViewFragment extends Fragment {
 
-    private RecyclerView recyclerView;
     private StaggeredRecyclerViewAdapter staggeredRecyclerViewAdapter;
     private static final int SPAN_COUNT = 2;
+    private FragmentGridBinding fragmentGridBinding;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        fragmentGridBinding = FragmentGridBinding.inflate(inflater);
+
         staggeredRecyclerViewAdapter = new StaggeredRecyclerViewAdapter();
         StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager
                 (SPAN_COUNT, LinearLayoutManager.VERTICAL);
 
-        recyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_grid, container, false);
         // To avoid item moving around
-        recyclerView.setItemAnimator(null);
-        recyclerView.setAdapter(staggeredRecyclerViewAdapter);
-        recyclerView.setLayoutManager(staggeredGridLayoutManager);
-        recyclerView.setHasFixedSize(true);
+        fragmentGridBinding.recyclerView.setItemAnimator(null);
+        fragmentGridBinding.recyclerView.setAdapter(staggeredRecyclerViewAdapter);
+        fragmentGridBinding.recyclerView.setLayoutManager(staggeredGridLayoutManager);
+        fragmentGridBinding.recyclerView.setHasFixedSize(true);
 
         MainActivityViewModel viewModel = ViewModelProviders.of(Objects.requireNonNull(getActivity())).get(MainActivityViewModel.class);
         viewModel.getPhotosLiveData().observe(getViewLifecycleOwner(), photos -> staggeredRecyclerViewAdapter.setPhotos(photos));
         // Inflate the layout for this fragment
-        return recyclerView;
+        return fragmentGridBinding.recyclerView;
     }
 
     @Override
@@ -63,19 +64,19 @@ public class GridViewFragment extends Fragment {
      */
     private void scrollToPosition() {
 
-        recyclerView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+        fragmentGridBinding.recyclerView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
             @Override
             public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft,
                                        int oldTop, int oldRight, int oldBottom) {
 
-                recyclerView.removeOnLayoutChangeListener(this);
-                final RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+                fragmentGridBinding.recyclerView.removeOnLayoutChangeListener(this);
+                final RecyclerView.LayoutManager layoutManager = fragmentGridBinding.recyclerView.getLayoutManager();
                 View viewAtPosition = Objects.requireNonNull(layoutManager).findViewByPosition(MainActivity.currentPosition);
                 // Scroll to position if the view for the current position is null (not currently part of
                 // layout manager children), or it's not completely visible.
                 if (viewAtPosition == null || layoutManager
                         .isViewPartiallyVisible(viewAtPosition, false, true)) {
-                    recyclerView.post(() -> layoutManager.scrollToPosition(MainActivity.currentPosition));
+                    fragmentGridBinding.recyclerView.post(() -> layoutManager.scrollToPosition(MainActivity.currentPosition));
                 }
             }
         });
@@ -85,7 +86,7 @@ public class GridViewFragment extends Fragment {
 
         try {
             //noinspection ConstantConditions
-            recyclerView.getLayoutManager().scrollToPosition(currentPosition);
+            fragmentGridBinding.recyclerView.getLayoutManager().scrollToPosition(currentPosition);
         } catch (Exception e) {
             e.printStackTrace();
         }

5ebba4c3c9387709fc854d8c0fbb713a65fab72b
kim00030 <ilhyungkim@hotmail.com>
2019-11-01 23:20:00 -0400

    updated

--- app/src/main/java/com/dan/traderevmobilechallenge/view/DummyClassC.java
+++ app/src/main/java/com/dan/traderevmobilechallenge/view/DummyClassC.java
@@ -11,9 +11,9 @@ public class DummyClassC {
 
     public DummyClassC(String s) {
         this.s = s;
-        int i = 1 +3;
-        String test = "Test";
+        int i = 1 +33;
+        String test = "Test2";
 
-        boolean myBoolean = false;
+        boolean myBoolean = true;
     }
 }
